358d603c7811d8820bf16176623431cd1443275d
Chris Williamson <chriswill0629@gmail.com>
2023-05-23 21:25:50 -0400

    Learning 10

--- Learning-11/main.go
+++ Learning-11/main.go
@@ -1,10 +1,23 @@
 package main
 
+import (
+	"github.com/cwilliamson29/GoLang-Learning/blob/main/Learning-11/helpers"
+	"log"
+)
+
+const numPool = 10
+
 func CalculateValue(intChan chan int) {
-	randomNumber := helpers.RandomNumber(10)
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
 }
 
 func main() {
 	intChan := make(chan int)
+	defer close(intChan)
 
+	go CalculateValue(intChan)
+
+	num := <-intChan
+	log.Println(num)
 }
